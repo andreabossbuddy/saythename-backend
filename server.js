6877f583a1666e646cb173118d8cb09471762662
teyamawi <teyaaaatawagon@gmail.com>
2025-10-22 03:33:55 +0800

    first commit

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 dotenv.config();
 
+import subscribeRoutes from './routes/subscribe.js';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -12,7 +13,7 @@ app.use(cors());
 app.use(json());
 
 // Routes
-app.use('/api', require('./routes/subscribe').default);
+app.use('/api', subscribeRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
